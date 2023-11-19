306d8d3f3091477d64d8cee1e54ad9a6db6b8e1c
imferr <117535778+imferr@users.noreply.github.com>
2023-11-19 01:21:03 -0400

    validando formulario de primera evaluacion

--- fithubwellness/src/components/FirstEvaluationPage.vue
+++ fithubwellness/src/components/FirstEvaluationPage.vue
@@ -2,43 +2,87 @@
     <div class="first-evaluation">
         <h1>BIENVENIDO A FITHUB!</h1>
         <h3>¿Primera vez aquí? ¡Completa algunos datos para comenzar!</h3>
-        <p>Para una atencion más personalizada y un mejor seguimiento de tu progreso, te pedimos que completes los siguientes datos. Esto nos ayudará a conocer tu estado actual y a diseñar un plan de entrenamiento que se adapte a ti y a tus objetivos.</p>
+        <p>Para una atencion más personalizada y un mejor seguimiento de tu progreso, te pedimos que completes los
+            siguientes datos. Esto nos ayudará a conocer tu estado actual y a diseñar un plan de entrenamiento que se adapte
+            a ti y a tus objetivos.</p>
         <form class="form-firstevaluation">
             <label for="weight">Peso:</label>
-            <input type="number" id="weight" name="weight">
+            <input type="number" id="weight" name="weight" v-model="weight">
 
             <label for="height">Altura (cm):</label>
-            <input type="number" id="height" name="height">
+            <input type="number" id="height" name="height" v-model="height">
 
             <label for="birthday">Cumpleaños:</label>
-            <input type="date" id="birthday" name="birthday">
+            <input type="date" id="birthday" name="birthday" v-model="birthday">
 
             <div class="save-first">
-                <router-link to="/"><SaveButton/></router-link>
+                <button class="save-button" type="button" @click="submitForm">Guardar</button>
             </div>
         </form>
     </div>
 </template>
   
 <script>
-import SaveButton from '../buttons/SaveButton.vue';
+import { ref, watchEffect } from 'vue';
+import { useAuth0 } from '@auth0/auth0-vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
 import Swal from 'sweetalert2';
 
 export default {
-  components: { SaveButton },
-  mounted() {
-    Swal.fire({
-      title: '¡Bienvenido a Fithub!',
-      text: 'Completa algunos datos para comenzar',
-      icon: 'success',
-      confirmButtonText: '¡Vamos!'
-    });
-  }
+    setup() {
+        const router = useRouter();
+        const { user, isAuthenticated } = useAuth0();
+        const isUserDataLoaded = ref(false);
+        const weight = ref(null);
+        const height = ref(null);
+        const birthday = ref(null);
+
+        watchEffect(() => {
+            if (isAuthenticated.value && user.value && user.value.name && user.value.email) {
+                isUserDataLoaded.value = true;
+            }
+        });
+
+        const submitForm = () => {
+            if (!isUserDataLoaded.value) {
+                alert("Los datos del usuario aún no están disponibles. Por favor, espera.");
+                return;
+            }
+
+            const userData = {
+                name: user.value.name,
+                email: user.value.email,
+                birthday: birthday.value
+            };
+
+            const evaluationData = {
+                weight: weight.value,
+                height: height.value
+            };
+
+            const requestData = {
+                user: userData,
+                evaluation: evaluationData
+            };
+
+            axios.post('http://localhost:8080/api/v1/user/createWithEvaluation', requestData)
+                .then(response => {
+                    Swal.fire('¡Listo!', 'Tu usuario y evaluación han sido creados', 'success');
+                    router.push({ name: 'home', params: { id: response.data.userId } });
+                })
+                .catch(error => {
+                    Swal.fire('Error', 'Hubo un error al crear el usuario y la evaluación: ' + error, 'error');
+                    console.error('Error al crear el usuario y la evaluación:', error);
+                });
+        };
+
+        return { weight, height, birthday, submitForm };
+    }
 };
 </script>
   
 <style>
-
 .first-evaluation {
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     width: 80%;
@@ -86,6 +130,5 @@ export default {
         width: 50%;
     }
 }
-
 </style>
   
